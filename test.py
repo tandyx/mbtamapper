a5b428e28bb2494f68c5615ed5c0270f4b81da69
Johan Cho <cho.joh@northeastern.edu>
2023-07-25 23:52:44 -0400

    fr final

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import schedule
 # from sqlalchemy import select
 
 from gtfs_loader import Feed
-from helper_functions import get_date
+from helper_functions import get_date, get_current_time
 
 load_dotenv()
 
@@ -45,16 +45,16 @@ def update_realtime(feed: Feed = None) -> None:
     Args:
         feed (Feed): GTFS feed (default: None)
     """
-    feed = feed or Feed(url=os.environ.get("GTFS_ZIP_LINK"), date=get_date(0))
+    feed = feed or Feed(url=os.environ.get("GTFS_ZIP_LINK"), date=get_current_time(-1))
     feed.import_realtime()
 
 
 if __name__ == "__main__":
     logging.getLogger().setLevel(logging.INFO)
     feed = Feed(url=os.environ.get("GTFS_ZIP_LINK"), date=get_date())
-    # # nightly_import(feed)
-    # # geojson_exports(feed)
-    update_realtime(feed)
+    # nightly_import(feed)
+    # geojson_exports(feed)
+    # update_realtime(feed)
     schedule.every(5).seconds.do(update_realtime)
     schedule.every().day.at("00:00", tz="America/New_York").do(nightly_import, None)
     schedule.every(60).minutes.at(":00", tz="America/New_York").do(
