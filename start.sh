e6f7348951026d782d9b4101e921c9c75e520991
Johan Cho <cho.joh@northeastern.edu>
2023-08-08 22:13:03 -0400

    add shell start

---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DEV=0
+
+HOST=0.0.0.0
+# SUBWAY="0 1"
+# RAPID_TRANSIT="0 1 4"
+# COMMUTER_RAIL=2
+# BUS=3
+# FERRY=4
+# ALL_ROUTES="0 1 2 3 4"
+# LIST_KEYS="SUBWAY RAPID_TRANSIT COMMUTER_RAIL BUS FERRY ALL_ROUTES"
+
+python -m venv ./.venv --upgrade-deps
+source ./.venv/bin/activate
+python -m pip install --upgrade pip
+python setup.py build_ext --force
+pip install --upgrade -r requirements.txt
+
+export PYTHONPATH="/helper_functions:/gtfs_schedule:/gtfs_realtime:/gtfs_loader"
+
+python test.py &
+
+if [ "$DEV" -ne 0 ]; then
+    python app.py &
+else
+    echo "dev mode is off"
+    waitress-serve --host $HOST --port 500 app:SW_APP &
+    waitress-serve --host $HOST --port 501 app:RT_APP &
+    waitress-serve --host $HOST --port 502 app:CR_APP &
+    waitress-serve --host $HOST --port 503 app:BUS_APP &
+    waitress-serve --host $HOST --port 504 app:FRR_APP &
+    waitress-serve --host $HOST --port 505 app:ALL_APP &
+fi
