b6dc6c5da3c2a0f34b71afac21a8e43feb8ce79c
Johan Cho <cho.joh@northeastern.edu>
2023-09-07 07:48:22 -0400

    update bat file

--- start.bat
+++ start.bat
@@ -5,9 +5,9 @@ title MBTA Mapper
 docker rm -vf $(docker ps -aq)
 docker rmi -vf $(docker ps -aq)
 docker build --tag mbta_mapper .
-docker run -d -p 80:80 mbta_mapper
-@REM docker tag mbta_mapper tandyy/mbta_mapper:latest
-@REM docker push tandyy/mbta_mapper:latest
+@REM docker run -d -p 80:80 mbta_mapper
+docker tag mbta_mapper tandyy/mbta_mapper:latest
+docker push tandyy/mbta_mapper:latest
 
 
 @REM set HOST=0.0.0.0
