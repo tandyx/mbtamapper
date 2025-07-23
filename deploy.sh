ee92d868d2eed7700e3b161d2eff06c23e67eb01
johan cho <johan-cho@outlook.com>
2025-07-23 01:01:37 -0400

    test something

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ cd static && npm install && cd ..
 
 sudo pkill .venv -f
 
-echo "starting mbtamapper!"
+echo "\n starting mbtamapper!"
 
 sudo .venv/bin/python3 -m waitress --listen=*:80 --threads=50 --call app:create_main_app &
 wait
