a83a991044deaa7784010ca81b6cd44c50c59969
johan cho <johan-cho@outlook.com>
2025-07-29 00:18:43 -0400

    add humanify

--- app.py
+++ app.py
@@ -66,20 +66,20 @@ def _error404(_app: flask.Flask, error: Exception | None = None) -> tuple[str, i
     return flask.render_template("404.html", key_dict=KEY_DICT, **url_dict), 404
 
 
-def register_humanify(_app: flask.Flask, **kwargs) -> Humanify:
-    """registers humanify to the specified app
+# def register_humanify(_app: flask.Flask, **kwargs) -> Humanify:
+#     """registers humanify to the specified app
 
-    Args:
-        app (flask.Flask): pointer to the flask app.
-        kwargs: extra args to Humanify class
+#     Args:
+#         app (flask.Flask): pointer to the flask app.
+#         kwargs: extra args to Humanify class
 
-    Returns:
-        Humanify: the registered object
-    """
+#     Returns:
+#         Humanify: the registered object
+#     """
 
-    humanify = Humanify(_app, challenge_type="one_click")
-    humanify.register_middleware(action="challenge", **kwargs)
-    return humanify
+#     humanify = Humanify(_app, challenge_type="one_click")
+#     humanify.register_middleware(action="challenge", **kwargs)
+#     return humanify
 
 
 def create_key_app(key: str, proxies: int = 5) -> flask.Flask:
@@ -192,7 +192,8 @@ def create_key_app(key: str, proxies: int = 5) -> flask.Flask:
         """
         return _error404(_app, error)
 
-    register_humanify(_app)
+    humanify = Humanify(_app, challenge_type="one_click")
+    humanify.register_middleware(action="challenge")
 
     if proxies:
         _app.wsgi_app = ProxyFix(
@@ -217,7 +218,7 @@ def create_main_app(import_data: bool = False, proxies: int = 5) -> flask.Flask:
 
     _app = flask.Flask(__name__)
 
-    register_humanify(_app)
+    # register_humanify(_app)
 
     with _app.app_context():  # background thread to run update
         thread = threading.Thread(
